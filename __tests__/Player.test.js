29573f185203ec4852bedaa0948805cc176da61c
Boba Fett <thomascupchurch@gmail.com>
2021-08-10 10:36:38 -0500

    add player test

---
+++ __tests__/Player.test.js
@@ -0,0 +1,11 @@
+const { TestWatcher } = require('jest');
+const Player = require('../lib/Player');
+
+TestWatcher('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+});
